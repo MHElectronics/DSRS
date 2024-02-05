50e414b64c30c8c2ad32591ee0cb311f8a3415f8
Muhammad Sajjad Hossain <sajjad.hossain@datasoft-bd.com>
2024-02-05 16:26:49 +0600

    Validation input field "invalid" class changed to "is-invalid" to match with template

---
+++ AxleLoadSystem/AxleLoadSystem/Helpers/CustomFieldClassProvider.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+namespace AxleLoadSystem.Helpers;
+
+public class CustomFieldClassProvider : FieldCssClassProvider
+{
+    public override string GetFieldCssClass(EditContext editContext,
+        in FieldIdentifier fieldIdentifier)
+    {
+        var isValid = editContext.IsValid(fieldIdentifier);
+
+        return isValid ? "valid" : "is-invalid";
+
+        //if (fieldIdentifier.FieldName == "Name")
+        //{
+        //    return isValid ? "validField" : "invalidField";
+        //}
+        //else
+        //{
+        //    if (editContext.IsModified(fieldIdentifier))
+        //    {
+        //        return isValid ? "modified valid" : "modified invalid";
+        //    }
+        //    else
+        //    {
+        //        return isValid ? "valid" : "invalid";
+        //    }
+        //}
+    }
+}
