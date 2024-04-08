84daa4695c87b6932273037e51bb1b285b5b0875
Muhammad Sajjad Hossain <sajjad.hossain@datasoft-bd.com>
2024-04-08 12:29:03 +0600

    AxleLoad column names corrected in datatable

--- AxleLoadSystem/AxleLoadSystem/Models/FileUploadModel.cs
+++ AxleLoadSystem/AxleLoadSystem/Models/FileUploadModel.cs
@@ -2,9 +2,7 @@
 
 namespace AxleLoadSystem.Models;
 
-public class FileUploadModel// : UploadedFile
+public class FileUploadModel : UploadedFile
 {
-    public int StationId { get; set; }
-    public DateTime Date { get; set; }
     public IFormFileCollection Attachments { get; set; }
 }
