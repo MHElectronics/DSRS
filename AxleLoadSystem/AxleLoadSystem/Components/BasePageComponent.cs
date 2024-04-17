5f15698f4f2cf283992f7e27393a683ca5f23706
Muhammad Sajjad Hossain <sajjad.hossain@datasoft-bd.com>
2024-04-17 14:29:03 +0600

    Theme state not working on page change (dark mode and menu open/collapse) issue solved

---
+++ AxleLoadSystem/AxleLoadSystem/Components/BasePageComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace AxleLoadSystem.Components;
+
+public abstract class BasePageComponent : ComponentBase
+{
+
+    [Inject]
+    protected IJSRuntime JSRuntime { get; set; }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            // run some JS function(s) here 
+            await JSRuntime.InvokeVoidAsync("SetThemeModes");
+        }
+    }
+}
