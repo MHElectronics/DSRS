d715ffd129a4a873e9b3d574fee0af079b7b2c83
mahmudul hasan <mahmudul.hasan@datasoft-bd.com>
2024-04-17 16:31:48 +0600

    Model Updated and Delete function init

--- BOL/LoadData.cs
+++ BOL/LoadData.cs
@@ -5,31 +5,31 @@ public class LoadData
     public int WIMId { get; set; }
     public int BRTAInfoId { get; set; }
 
-    public int TransactionNumber { get; set; }
+    public string TransactionNumber { get; set; }
     public int StationCode { get; set; }
     public int LaneNumber { get; set; }
-    public int DateTime { get; set; }
+    public DateTime DateTime { get; set; }
     public string PlateZone { get; set; }
     public string PlateSeries { get; set; }
     public string PlateNumber { get; set; }
     public int VehicleId { get; set; }
     public int NumberOfAxle { get; set; }
-    public int Axle1st { get; set; }
-    public int Axle2nd { get; set; }
-    public int Axle3rd { get; set; }
-    public int Axle4th { get; set; }
-    public int Axle5th { get; set; }
-    public int Axle6th { get; set; }
-    public int Axle7th { get; set; }
-    public int AxleRemaining { get; set; }
-    public int GrossVehicleWeight { get; set; }
-    public int FinePayment { get; set; }
-    public int ReceiptNumber { get; set; }
-    public int BillNumber { get; set; }
-    public int IsUnloaded { get; set; }
-    public int IsOverloaded { get; set; }
-    public int OverSizedModified { get; set; }
-    public int Wheelbase { get; set; }
+    public decimal Axle1st { get; set; }
+    public decimal Axle2nd { get; set; }
+    public decimal Axle3rd { get; set; }
+    public decimal Axle4th { get; set; }
+    public decimal Axle5th { get; set; }
+    public decimal Axle6th { get; set; }
+    public decimal Axle7th { get; set; }
+    public decimal AxleRemaining { get; set; }
+    public decimal GrossVehicleWeight { get; set; }
+    public decimal FinePayment { get; set; }
+    public string ReceiptNumber { get; set; }
+    public string BillNumber { get; set; }
+    public bool IsUnloaded { get; set; }
+    public bool IsOverloaded { get; set; }
+    public bool OverSizedModified { get; set; }
+    public decimal Wheelbase { get; set; }
 
     //Slow Moving
     public decimal VehicleSpeed { get; set; }
