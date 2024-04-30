9eb9c1739ac6173e463a05934cf187c318cfc853
Muhammad Sajjad Hossain <sajjad.hossain@datasoft-bd.com>
2024-04-30 10:59:35 +0600

    minor changes

--- BOL/WIMScale.cs
+++ BOL/WIMScale.cs
@@ -3,7 +3,7 @@ public class WIMScale
 {
     public int Id { get; set; }
     public int StationId { get; set; }
-    public int LaneNumber { get; set; }
+    public int LaneNumber { get; set; } = 1;
     public bool IsHighSpeed { get; set; }
     public string EquipmentCode { get; set; }
     public string LaneDirection { get; set; }
