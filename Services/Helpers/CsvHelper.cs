1d2e9c5f7be7792d62be4fae231b828d32613350
mahmudul hasan <mahmudul.hasan@datasoft-bd.com>
2024-06-19 18:39:57 +0600

    Lane Number issue updated

--- Services/Helpers/CsvHelper.cs
+++ Services/Helpers/CsvHelper.cs
@@ -94,7 +94,7 @@ public class CsvHelper : ICsvHelper
         dt.Columns.Add("FileId", typeof(int));
 
         dt.Columns.Add(NewDataColumn("TransactionNumber", typeof(string), 10));
-        dt.Columns.Add(NewDataColumn("LaneNumber", typeof(int)));
+        dt.Columns.Add(NewDataColumn("LaneNumber", typeof(string),2));
         dt.Columns.Add(NewDataColumn("DateTime", typeof(DateTime)));
         dt.Columns.Add(NewDataColumn("PlateZone", typeof(string), 50));
         dt.Columns.Add(NewDataColumn("PlateSeries", typeof(string), 10));
