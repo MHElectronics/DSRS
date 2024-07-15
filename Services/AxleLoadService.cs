1ea815cf2740d1349f078ccee696e9c9e6eb7f6a
Muhammad Sajjad Hossain <sajjad.hossain@datasoft-bd.com>
2024-07-15 15:15:37 +0600

    Add load data return type changed

--- Services/AxleLoadService.cs
+++ Services/AxleLoadService.cs
@@ -28,9 +28,10 @@ public class AxleLoadService(ISqlDataAccess _db) : IAxleLoadService
 
         return await _db.LoadData<LoadData, object>(query, obj);
     }
-    public async Task<bool> Add(LoadData obj)
+    public async Task<(bool,string)> Add(LoadData obj)
     {
         bool isSuccess = false;
+        string message = "";
         string query = @"INSERT INTO AxleLoad(StationId,TransactionNumber,LaneNumber,DateTime,PlateZone,PlateSeries,PlateNumber,VehicleId,NumberOfAxle,VehicleSpeed
             ,Axle1,Axle2,Axle3,Axle4,Axle5,Axle6,Axle7
             ,AxleRemaining,GrossVehicleWeight,IsUnloaded,IsOverloaded,OverSizedModified,Wheelbase,ClassStatus,RecognizedBy,IsBRTAInclude,LadenWeight,UnladenWeight,ReceiptNumber,BillNumber
@@ -50,13 +51,15 @@ public class AxleLoadService(ISqlDataAccess _db) : IAxleLoadService
             if(ex.Number == 2601)
             {
                 isSuccess = false;
+                message = "Duplicate data";
             }
         }
         catch (Exception ex)
         {
             isSuccess = false;
+            message = "Error: " + ex.Message;
         }
-        return isSuccess;
+        return (isSuccess, message);
     }
     public async Task<bool> Add(List<LoadData> obj)
     {
