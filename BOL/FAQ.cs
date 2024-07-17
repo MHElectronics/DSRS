d3dd221e73a71d16bbf438faedc91a10fb71c4d7
Muhammad Sajjad Hossain <sajjad.hossain@datasoft-bd.com>
2024-07-17 21:42:39 +0600

    FAQ page updated

---
+++ BOL/FAQ.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOL;
+public class FAQ
+{
+    public int Id { get; set; }
+    public string Question { get; set; }
+    public string Answer { get; set; }
+    public int UserId {  get; set; }
+    public bool IsPublished { get; set; }
+}
