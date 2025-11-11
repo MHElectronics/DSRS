1384de58208ebe32af09f59e1adc68eb8a7687bc
mahmudul hasan <mahmudul.hasan@datasoft-bd.com>
2025-11-11 10:12:31 +0600

    draft customer, dcu, meter init

--- Services/CustomerService.cs
+++ Services/CustomerService.cs
@@ -1,4 +1,79 @@
-﻿namespace Services;
-public class CustomerService
+﻿using BOL;
+using Services.Helpers;
+namespace Services;
+public interface ICustomerService
 {
+    Task<IEnumerable<Customer>> GetcustomerList();
+    Task<Customer> GetcustomerById(int id);
+    Task<bool> Insertcustomer(Customer customer, User user);
+    Task<Customer> Updatecustomer(Customer customer, User user);
+    Task<bool> Deletecustomer(int id, User user);
+
+}
+public class CustomerService : ICustomerService
+{
+    private readonly ISqlDataAccess _db;
+    private readonly IUserActivityService _userActivityService;
+
+    public CustomerService(ISqlDataAccess db, IUserActivityService userActivityService)
+    {
+        _db = db;
+        _userActivityService = userActivityService;
+    }
+    public async Task<Customer> GetcustomerById(int id)
+    {
+        IEnumerable<Customer>? results = await _db.LoadData<Customer, dynamic>("SELECT * FROM Customer WHERE Id=@Id", new { Id = id });
+        return results.FirstOrDefault();
+    }
+    public async Task<IEnumerable<Customer>> GetcustomerList() =>
+        await _db.LoadData<Customer, dynamic>("SELECT * FROM Customer", new { });
+
+    public async Task<bool> Insertcustomer(Customer customer, User user)
+    {
+        bool hasDuplicate = await this.CheckDuplicatecustomer(customer);
+        if (!hasDuplicate)
+        {
+            string sql = @"INSERT INTO customer(Name,Description,Address,ContactNo,MeterType) VALUES(@Name,@Description,@Address,@ContactNo,@MeterType)";
+            bool isSuccess = await _db.SaveData<Customer>(sql, customer);
+
+            if (isSuccess)
+            {
+                UserActivity log = new UserActivity(user.Id, customer.Name + "customer Added", LogActivity.Insert);
+                await _userActivityService.InsertUserActivity(log);
+            }
+
+            return isSuccess;
+        }
+        return false;
+    }
+
+    public async Task<Customer> Updatecustomer(Customer customer, User user)
+    {
+        string sql = @"UPDATE customer SET Name=@Name, Description=@Description, Address=@Address, ContactNo=@ContactNo, MeterType=@MeterType  WHERE Id=@Id";
+        await _db.SaveData(sql, customer);
+
+        UserActivity log = new UserActivity(user.Id, customer.Name + "customer Updated", LogActivity.Update);
+        await _userActivityService.InsertUserActivity(log);
+
+        return customer;
+    }
+    public async Task<bool> Deletecustomer(int id, User user)
+    {
+        string query = "DELETE FROM customer WHERE Id=@Id";
+        int count = await _db.DeleteData<Customer, object>(query, new { id });
+
+        if (count > 0)
+        {
+            UserActivity log = new UserActivity(user.Id, "customer Delete", LogActivity.Delete);
+            await _userActivityService.InsertUserActivity(log);
+        }
+        return count > 0;
+    }
+
+
+    private async Task<bool> CheckDuplicatecustomer(Customer customer)
+    {
+        string query = "SELECT COUNT(1) Count FROM customer WHERE(LOWER(Name)=LOWER(@Name) OR Id=@Id)";
+        return await _db.LoadSingleAsync<bool, object>(query, customer);
+    }
 }
---
+++ Services/TransactionService.cs
@@ -0,0 +1,79 @@
+﻿using BOL;
+using Services.Helpers;
+namespace Services;
+public interface ITransactionService
+{
+    Task<IEnumerable<Transaction>> GetTransactionList();
+    Task<Transaction> GetTransactionById(int id);
+    Task<bool> InsertTransaction(Transaction Transaction, User user);
+    Task<Transaction> UpdateTransaction(Transaction Transaction, User user);
+    Task<bool> DeleteTransaction(int id, User user);
+
+}
+public class TransactionService : ITransactionService
+{
+    private readonly ISqlDataAccess _db;
+    private readonly IUserActivityService _userActivityService;
+
+    public TransactionService(ISqlDataAccess db, IUserActivityService userActivityService)
+    {
+        _db = db;
+        _userActivityService = userActivityService;
+    }
+    public async Task<Transaction> GetTransactionById(int id)
+    {
+        IEnumerable<Transaction>? results = await _db.LoadData<Transaction, dynamic>("SELECT * FROM Transaction WHERE Id=@Id", new { Id = id });
+        return results.FirstOrDefault();
+    }
+    public async Task<IEnumerable<Transaction>> GetTransactionList() =>
+        await _db.LoadData<Transaction, dynamic>("SELECT * FROM Transaction", new { });
+
+    public async Task<bool> InsertTransaction(Transaction Transaction, User user)
+    {
+        bool hasDuplicate = await this.CheckDuplicateTransaction(Transaction);
+        if (!hasDuplicate)
+        {
+            string sql = @"INSERT INTO Transaction(CustomerId,CustomerName,TransactionId,TransactionTime,Type,Narration,IsSystemGenerated,Status,MeterNumber,Amount) VALUES(@CustomerId,@CustomerName,@TransactionId,@TransactionTime,@Type,@Narration,@IsSystemGenerated,@Status,@MeterNumber,@Amount)";
+            bool isSuccess = await _db.SaveData<Transaction>(sql, Transaction);
+
+            if (isSuccess)
+            {
+                UserActivity log = new UserActivity(user.Id, Transaction.Id + "Transaction Added", LogActivity.Insert);
+                await _userActivityService.InsertUserActivity(log);
+            }
+
+            return isSuccess;
+        }
+        return false;
+    }
+
+    public async Task<Transaction> UpdateTransaction(Transaction Transaction, User user)
+    {
+        string sql = @"UPDATE Transaction SET CustomerId=@CustomerId, CustomerName=@CustomerName, TransactionId=@TransactionId, TransactionTime=@TransactionTime, Type=@Type, Narration=@Narration, IsSystemGenerated=@IsSystemGenerated, Status=@Status, MeterNumber=@MeterNumber, Amount=@Amount   WHERE Id=@Id";
+        await _db.SaveData(sql, Transaction);
+
+        UserActivity log = new UserActivity(user.Id, Transaction.Id + "Transaction Updated", LogActivity.Update);
+        await _userActivityService.InsertUserActivity(log);
+
+        return Transaction;
+    }
+    public async Task<bool> DeleteTransaction(int id, User user)
+    {
+        string query = "DELETE FROM Transaction WHERE Id=@Id";
+        int count = await _db.DeleteData<Transaction, object>(query, new { id });
+
+        if (count > 0)
+        {
+            UserActivity log = new UserActivity(user.Id, "Transaction Delete", LogActivity.Delete);
+            await _userActivityService.InsertUserActivity(log);
+        }
+        return count > 0;
+    }
+
+
+    private async Task<bool> CheckDuplicateTransaction(Transaction Transaction)
+    {
+        string query = "SELECT COUNT(1) Count FROM Transaction WHERE(LOWER(TransactionId)=LOWER(@TransactionId) OR Id=@Id)";
+        return await _db.LoadSingleAsync<bool, object>(query, Transaction);
+    }
+}
