4d88f3595b87bcc911e47cb1a071c0a8ef387358
mahmudul hasan <hasan168989@gmail.com>
2025-11-14 02:01:31 +0600

    DCU, Meter and customer view correction

--- BOL/Meter.cs
+++ BOL/Meter.cs
@@ -3,10 +3,10 @@ public class Meter
 {
     public int Id { get; set; }
     public string MeterNumber { get; set; }
-    public string MeterAddress { get; set; }
+    public string Address { get; set; }
     public MeterType MeterType { get; set; }
     public int CompanyId { get; set; }
-    public int DCUId { get; set; }
+    public int Dcuid { get; set; }
     public int CustomerId { get; set; }
     public bool IsActive { get; set; }
 }
